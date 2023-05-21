2b157b74d0db126272343805d3195b59b98ff827
Darron Froese <darron@froese.org>
2023-05-21 10:09:32 -0600

    Add some tests.

--- core/news_story_test.go
+++ core/news_story_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -8,5 +10,20 @@ func TestFakeNewsStory(t *testing.T) {
 	j := FakeNewsStoryJSON()
 	if j == "" {
 		t.Error("JSON was blank")
+	}
+}
+
+func TestUnmarkshalJSONNewsStory(t *testing.T) {
+	r := FakeNewsStory()
+	j, err := json.Marshal(r)
+	if err != nil {
+		t.Error(err)
+	}
+	r2, err := UnmarshalJSONNewsStory(string(j))
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(r, r2) {
+		t.Error("Those should match")
 	}
 }
--- core/record_test.go
+++ core/record_test.go
@@ -1,6 +1,8 @@
 package core
 
 import (
+	"encoding/json"
+	"reflect"
 	"testing"
 
 	"github.com/go-faker/faker/v4"
@@ -18,5 +20,20 @@ func TestRecordFakerJSON(t *testing.T) {
 	j := FakeRecordJSON()
 	if j == "" {
 		t.Error("JSON was blank")
+	}
+}
+
+func TestUnmarkshalJSONRecord(t *testing.T) {
+	r := FakeRecord()
+	j, err := json.Marshal(r)
+	if err != nil {
+		t.Error(err)
+	}
+	r2, err := UnmarshalJSONRecord(string(j))
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(r, r2) {
+		t.Error("Those should match")
 	}
 }
