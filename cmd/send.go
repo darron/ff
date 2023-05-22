e22ae777f45cadc5a01d0e25ee3a90d8b0eddb03
Darron Froese <darron@froese.org>
2023-05-22 12:07:33 -0600

    Move functions to more central location.

--- cmd/send.go
+++ cmd/send.go
@@ -2,7 +2,12 @@ package cmd
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
 
+	"github.com/gojek/heimdall/v7"
+	"github.com/gojek/heimdall/v7/httpclient"
 	"github.com/spf13/cobra"
 )
 
@@ -20,4 +25,36 @@ var sendCmd = &cobra.Command{
 
 func doSend() {
 	fmt.Println("Do the HTTP send here")
+}
+
+func getHTTPClient() *httpclient.Client {
+	// Create an HTTP client.
+	initalTimeout := 2 * time.Millisecond         // Inital timeout
+	maxTimeout := 9 * time.Millisecond            // Max time out
+	exponentFactor := 2                           // Multiplier
+	maximumJitterInterval := 2 * time.Millisecond // Max jitter interval. It must be more than 1*time.Millisecond
+
+	backoff := heimdall.NewExponentialBackoff(initalTimeout, maxTimeout, float64(exponentFactor), maximumJitterInterval)
+
+	// Create a new retry mechanism with the backoff
+	retrier := heimdall.NewRetrier(backoff)
+
+	// Create a new client, sets the retry mechanism, and the number of times you would like to retry
+	client := httpclient.NewClient(
+		httpclient.WithHTTPTimeout(httpTimeout),
+		httpclient.WithRetrier(retrier),
+		httpclient.WithRetryCount(4),
+	)
+	return client
+}
+
+func getHTTPRequest(method, url, body string) *http.Request {
+	var req *http.Request
+	if body != "" {
+		req, _ = http.NewRequest(method, url, strings.NewReader(body))
+	} else {
+		req, _ = http.NewRequest(method, url, nil)
+	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	return req
 }
--- cmd/send_fake_record.go
+++ cmd/send_fake_record.go
@@ -4,13 +4,9 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"strings"
-	"time"
 
 	"github.com/darron/ff/config"
 	"github.com/darron/ff/core"
-	"github.com/gojek/heimdall/v7"
-	"github.com/gojek/heimdall/v7/httpclient"
 	"github.com/spf13/cobra"
 )
 
@@ -31,33 +27,6 @@ var sendFakeRecordCmd = &cobra.Command{
 			log.Fatal(err)
 		}
 	},
-}
-
-func getHTTPClient() *httpclient.Client {
-	// Create an HTTP client.
-	initalTimeout := 2 * time.Millisecond         // Inital timeout
-	maxTimeout := 9 * time.Millisecond            // Max time out
-	exponentFactor := 2                           // Multiplier
-	maximumJitterInterval := 2 * time.Millisecond // Max jitter interval. It must be more than 1*time.Millisecond
-
-	backoff := heimdall.NewExponentialBackoff(initalTimeout, maxTimeout, float64(exponentFactor), maximumJitterInterval)
-
-	// Create a new retry mechanism with the backoff
-	retrier := heimdall.NewRetrier(backoff)
-
-	// Create a new client, sets the retry mechanism, and the number of times you would like to retry
-	client := httpclient.NewClient(
-		httpclient.WithHTTPTimeout(httpTimeout),
-		httpclient.WithRetrier(retrier),
-		httpclient.WithRetryCount(4),
-	)
-	return client
-}
-
-func getHTTPRequest(method, url, body string) *http.Request {
-	req, _ := http.NewRequest(method, url, strings.NewReader(body))
-	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
-	return req
 }
 
 func sendFakeRecord(conf *config.App) error {
