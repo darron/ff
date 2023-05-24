fcc0a47716e360111af501839d8ef00579e2d574
Darron Froese <darron@froese.org>
2023-05-24 17:05:09 -0600

    Remove flag from config - simplify - moving config to cmd.

--- config/config.go
+++ config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"flag"
 	"fmt"
 	"os"
 
@@ -28,7 +27,6 @@ var (
 	defaultLogLevel  = "debug"
 	defaultPort      = "8080"
 	defaultRedis     = "127.0.0.1:6379"
-	defaultStorage   = "redis"
 )
 
 func defaultOpts() Opts {
@@ -37,52 +35,34 @@ func defaultOpts() Opts {
 	}
 }
 
-func withRedis(conn string) OptFunc {
+func WithRedis(conn string) OptFunc {
 	return func(opts *Opts) {
 		opts.RecordRepository = redis.NewRecordRepository(conn)
 		opts.NewsStoryRepository = redis.NewNewsStoryRepository(conn)
 	}
 }
 
-func withLogger(l *slog.Logger) OptFunc {
+func WithLogger(level, format string) OptFunc {
+	l := GetLogger(level, format)
 	return func(opts *Opts) {
 		opts.Logger = l
 	}
 }
 
-func setPort(port string) OptFunc {
+func WithPort(port string) OptFunc {
 	return func(opts *Opts) {
 		opts.Port = port
 	}
 }
 
 func New() (*App, error) {
-	var logLevel string
-	var logformat string
 	var optFuncs []OptFunc
-	var port string
-	var redis string
-	var storage string
 
-	flag.StringVar(&logLevel, "loglevel", defaultLogLevel, "Default Log Level")
-	flag.StringVar(&logformat, "logformat", defaultLogformat, "Log format: json or text")
-	flag.StringVar(&port, "port", defaultPort, "HTTP Port")
-	flag.StringVar(&redis, "redis", defaultRedis, "Redis Connection String")
-	flag.StringVar(&storage, "storage", defaultStorage, "Storage for Data")
-
-	flag.Parse()
-
-	// Let's deal with logging immediately
-	logger := GetLogger(logLevel, logformat)
-	optFuncs = append(optFuncs, withLogger(logger))
-
-	// If we've picked other options - add them to optFuncs
-	if port != defaultPort {
-		optFuncs = append(optFuncs, setPort(port))
-	}
-	if storage == "redis" && redis != "" {
-		optFuncs = append(optFuncs, withRedis(redis))
-	}
+	// Really basic default options without any configuration.
+	// Moving configuration to cmd and will be calling `Get(WithOption())`
+	optFuncs = append(optFuncs, WithLogger(defaultLogLevel, defaultLogformat))
+	optFuncs = append(optFuncs, WithPort(defaultPort))
+	optFuncs = append(optFuncs, WithRedis(defaultRedis))
 
 	return Get(optFuncs...)
 }
